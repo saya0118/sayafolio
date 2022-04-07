990a3bfbb1fee83bfa3eb67c881619b367e1fa2a
saya0118 <chemi0802@gmail.com>
2022-04-06 19:47:59 -0700

    added image

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import styles from "./Header.module.css";
+import svgLogo from "../../image/logo.svg";
 
 export const Header = () => {
-    return (
-        <div className={styles.header}>
-        <div className={styles['nav-bar']}>
-            <ul>
-              <li className={styles['nav-left']}><a>Sayaka Matsuda</a></li>
-              <li><a>HOME</a></li>
-              <li><a>ABOUT</a></li>
-              <li><a>WORKS</a></li>
-              <li><a>SKILLS</a></li>
-              <li><a>CONTACT</a></li>
-            </ul>
+  return (
+    <div className={styles.header}>
+      <div className={styles["nav-bar"]}>
+        <div className={styles["nav-left"]}>
+          <ul>
+            <li>
+              <img src={svgLogo} alt="sayaka's logo" className={styles.logo} />
+            </li>
+            <li>
+              <p>Sayaka Matsuda</p>
+            </li>
+          </ul>
         </div>
-        <div className={styles['app-header']}>
-          <p className={styles['greeting']}>Hi there, I'm Saya!<br/>React Developer based in Vancouver</p>
+        <div className={styles["nav-right"]}>
+          <ul>
+            <li>
+              <a>HOME</a>
+            </li>
+            <li>
+              <a>ABOUT</a>
+            </li>
+            <li>
+              <a>WORKS</a>
+            </li>
+            <li>
+              <a>SKILLS</a>
+            </li>
+            <li>
+              <a>CONTACT</a>
+            </li>
+          </ul>
         </div>
       </div>
-    )
-}
+      <div className={styles["greeting-container"]}>
+        <p className={styles["greeting"]}>
+          Hi there, I'm Saya!
+          <br />
+          Frontend Developer based in Vancouver
+        </p>
+      </div>
+    </div>
+  );
+};
