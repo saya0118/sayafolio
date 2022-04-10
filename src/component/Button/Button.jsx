512f7abf1f722e98a0c5d9bd88a5a55a786bc574
saya0118 <chemi0802@gmail.com>
2022-04-09 21:52:52 -0700

    added new className for different sizes

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import styles from "./Button.module.css";
 import { useColor } from "../Context/Context";
 
-export const Button = ({text}) => {
+export const Button = ({text, href, size}) => {
   const darkMode = useColor();
   return (
     <div className={styles["resume-button"]}>
       <a
-        className={`${styles["resume-link"]} ${
+        className={`${styles["resume-link"]} ${styles[size]} ${
           darkMode ? styles["resume-link-dark"] : styles["resume-link-light"]
         }`}
-        href="#"
+        href={href}
       >
         <span className={styles["resume-title"]}>{text}</span>
       </a>
