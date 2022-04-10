d19ff5e35238ebc97b2f9abf35dab1cd2ef0301f
saya0118 <chemi0802@gmail.com>
2022-04-09 17:59:02 -0700

    adjusted dark mode

--- src/component/Social/Social.jsx
+++ src/component/Social/Social.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import styles from "./Social.module.css";
+import { useColor } from "../Context/Context";
 import iconGithub from "../../image/brown-github.svg";
 import iconLinkedin from "../../image/brown-linkedin.svg";
 import iconMail from "../../image/brown-mail.svg";
+import darkGithub from "../../image/github-dark.svg";
+import darkLinkedin from "../../image/linkedin-dark.svg";
+import darkMail from "../../image/mail-dark.svg";
 
 export const Social = () => {
+  const darkMode = useColor();
+  const themeStyles = {
+    backgroundColor: darkMode
+      ? "var(--dark-bg-color)"
+      : "var(--main-color-beige)",
+    color: darkMode ? "white" : "var(--main-color-charcoal",
+  };
   return (
     <div className={styles.social}>
       <ul className={styles["social-ul"]}>
         <li className={styles["social-list"]}>
-          <button className={styles["social-button"]}>
+          <button style={themeStyles} className={styles["social-button"]}>
             <img
               className={styles["social-img"]}
-              src={iconLinkedin}
+              src={darkMode ? darkLinkedin : iconLinkedin}
               alt="linked in"
             />
             <a className={styles["social-link"]} href="" />
@@ -20,21 +31,28 @@ export const Social = () => {
         </li>
         <li className={styles["social-list"]}>
           <a target="_blank" href="https://github.com/saya0118">
-            <button className={styles["social-button"]}>
+            <button style={themeStyles} className={styles["social-button"]}>
               <img
                 className={styles["social-img"]}
-                src={iconGithub}
+                src={darkMode ? darkGithub : iconGithub}
                 alt="github"
               />
             </button>
           </a>
         </li>
         <li className={styles["social-list"]}>
-          <a target="_blank" onClick={() => window.location = 'mailto:chemi0802@gmail.com'}>
-            <button className={styles["social-button"]} type="button">
+          <a
+            target="_blank"
+            onClick={() => (window.location = "mailto:chemi0802@gmail.com")}
+          >
+            <button
+              style={themeStyles}
+              className={styles["social-button"]}
+              type="button"
+            >
               <img
                 className={styles["social-img"]}
-                src={iconMail}
+                src={darkMode ? darkMail : iconMail}
                 alt="gmail"
               />
             </button>
