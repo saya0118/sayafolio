c155cde55c49139fc4b203a713dc17a207902970
saya0118 <chemi0802@gmail.com>
2022-04-09 17:43:38 -0700

    created app component

---
+++ src/component/App/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./App.module.css";
+import { useColor } from "../Context/Context";
+import { Header } from "../Header";
+import { Profile } from "../Profile";
+import { Skill } from "../Skill";
+import { Work } from "../Work";
+import { Contact } from "../Contact";
+
+export function App() {
+  const darkMode = useColor();
+  const themeStyles = {
+    backgroundColor: darkMode
+      ? "var(--dark-bg-color)"
+      : "var(--main-color-beige)",
+    color: darkMode ? "white" : "var(--main-color-charcoal)",
+  };
+  return (
+    <div style={themeStyles} className={styles.app}>
+      <Header />
+      <Profile />
+      <Skill />
+      <Work />
+      <Contact />
+    </div>
+  );
+}
